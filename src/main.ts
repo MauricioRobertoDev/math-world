09677bc9014369abb239b5cdc40a7f041450698e
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2023-06-04 09:15:46 -0300

    exportação de tipos e classe paint para facilitar a construção de funções recebendo o paint

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import MathWorld from "./classes/math-world";
 import Vector2D from "./classes/vector-2d";
+import Paint from "./classes/paint";
 
-export { MathWorld, Vector2D };
+export { MathWorld, Vector2D, Paint };
+export type { Point, DebugMouseMode, PaintDrawMode } from "./types";
